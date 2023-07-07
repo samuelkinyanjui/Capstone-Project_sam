3eb5ef235993128e14476909d7975178d80dfd28
Samuel Kinyanjui <sax80kinya@gmail.com>
2023-07-07 17:16:19 +0300

    updated captstone project
    We have created
    The home page.
    The about page.
    The home page should have a link in the menu to the about page.
    The logo in the header links to the home page
    Featured speakers is dynamically in JavaScript.
    this is the link to my vedio explaining the process
    https://www.loom.com/share/13b08eb080734a92b887de793104f138?sid=7bf0a263-a0a2-4837-b97c-0efa6f3a47f9

---
+++ script.js
@@ -0,0 +1,80 @@
+const speakers = [
+    {
+        id: 1,
+        speakerName: 'SAMUEL KINYAKUI',
+        speakerPosition: 'CYBERd Software Developer',
+        imgSource:'assets/speakers/1.jpg',
+        description: 'He has Studied Cyber security at East London univesrity collage UK.Studied virus manupulation.Work for Equity bank Africa',
+    },
+    {
+        id: 2,
+        speakerName: 'SAMUEL KINYAKUI',
+        speakerPosition: 'Database General Manager',
+        imgSource:'assets/speakers/2.jpg',
+        description: 'He has Studied Cyber security at East London univesrity collage UK.Studied virus manupulation.Work for Equity bank Africa',
+    },
+    {
+        id: 3,
+        speakerName: 'SAMUEL KINYAKUI',
+        speakerPosition: 'Full Stack Developer',
+        imgSource:'assets/speakers/3.jpg',
+        description: 'He has Studied Cyber security at East London univesrity collage UK.Studied virus manupulation.Work for Equity bank Africa',
+    },
+    {
+        id: 4,
+        speakerName: 'SAMUEL KINYAKUI',
+        speakerPosition: 'Database and Software Developer',
+        imgSource:'assets/speakers/1.jpg',
+        description: 'He has Studied Cyber security at East London univesrity collage UK.Studied virus manupulation.Work for Equity bank Africa',
+    },
+    {
+        id: 5,
+        speakerName: 'M. SAMUEL KINYAKUI',
+        speakerPosition: 'Database General Manager',
+        imgSource:'assets/speakers/2.jpg',
+        description: 'He has Studied Cyber security at East London univesrity collage UK.Studied virus manupulation.Work for Equity bank Africa',
+    },
+    {
+        id: 6,
+        speakerName: 'SAMUEL KINYAKUI',
+        speakerPosition: 'Full Stack Developer',
+        imgSource:'assets/speakers/3.jpg',
+        description: 'He has Studied Cyber security at East London univesrity collage UK.Studied virus manupulation.Work for Equity bank Africa',
+    },
+   
+];
+
+speakers.forEach((speaker) => {
+    const speakerSection = document.getElementById('mainSection');
+    const partDiv = document.createElement('div');
+    partDiv.className = 'col-md-5';
+    partDiv.className = 'speaker-card';
+    partDiv.innerHTML = `
+        <div class="speaker">
+                <img src="${speaker.imgSource}" alt="speaker image" class="img-fluid speaker-photo">
+            </div> 
+            <div class="speaker-info">
+                <h1 class="name">${speaker.speakerName}</h1>
+                <p class="speaker-job">${speaker.speakerPosition}</p>
+                <div class="left-line"></div>
+                <p class="speaker-exp">${speaker.description}</p>
+            </div>`;
+
+    speakerSection.append(partDiv);
+});
+
+function ShowXicon() {
+    const iconMenu = document.getElementById('meneIcon');
+    const iconX = document.getElementById('Xicon');
+    
+    iconX.style.display = 'inline';
+    iconMenu.style.display = 'none';
+}
+
+function removeXiocn() {
+    const iconMenu = document.getElementById('meneIcon');
+    const iconX = document.getElementById('Xicon');
+
+    iconX.style.display = 'none';
+    iconMenu.style.display = 'block';
+}
